069ee95ae493a8b58676111749ad080e79ef22d8
architect <ng_sou@163.com>
2021-06-01 04:22:54 +0800

    增加设计模式

--- DesignPattern/创建型/Singleton.php
+++ DesignPattern/创建型/Singleton.php
@@ -3,5 +3,30 @@
 
 class Singleton
 {
+    private static $instance = null;
 
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * 构造函数私有化
+     * Singleton constructor.
+     */
+    private function __construct(){}
+
+    /**
+     * 防止对象被克隆
+     */
+    private function __clone(){}
+
+    /**
+     * 防止被反序列化
+     */
+    private function __wakeup(){}
 }
