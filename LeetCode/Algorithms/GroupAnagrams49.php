302357f5d073cf896d4e71ec454a3830550c0042
architectPPN <ng_sou@163.com>
2022-12-12 21:50:02 +0800

    新增：字母异位词分组

---
+++ LeetCode/Algorithms/GroupAnagrams49.php
@@ -0,0 +1,29 @@
+<?php
+
+## 解法1：排序
+
+$question = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+$answer = [];
+foreach ($question as $value) {
+    $sortStr = sortStr($value);
+
+    $answer[$sortStr][] = $value;
+}
+
+/**
+ * 字符串排序
+ *
+ * @param string $waitString 等待排序的字符串
+ * @return string
+ */
+function sortStr(string $waitString): string
+{
+    $strArr = str_split($waitString);
+
+    asort($strArr);
+
+    return implode("", $strArr);
+}
+
+var_dump($answer);
