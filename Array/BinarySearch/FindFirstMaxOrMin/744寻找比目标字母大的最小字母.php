658719642b36401a8b7af49a5c6411f1897a39a4
architectppn <ng_sou@163.com>
2024-06-09 13:40:16 +0800

    278 744

---
+++ Array/BinarySearch/FindFirstMaxOrMin/744寻找比目标字母大的最小字母.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NextGreatestLetter;
+
+class Solution {
+    /**
+     * @param String[] $letters
+     * @param String $target
+     * @return String
+     */
+    function nextGreatestLetter(array $letters,string $target): string
+    {
+        $arrLength = count($letters) - 1;
+        if ($target >= $letters[$arrLength]) {
+            return $letters[0];
+        }
+
+        $left = 0;
+        while ($left < $arrLength) {
+            $mid = $left + intdiv($arrLength - $left, 2);
+            if ($letters[$mid] > $target) {
+                $arrLength = $mid;
+            } else {
+                $left = $mid + 1;
+            }
+        }
+
+        return $letters[$left];
+    }
+}
+
+$letters = ["c","f","j"];
+$target = 'a';
+$solution = new Solution();
+echo $solution->nextGreatestLetter($letters, $target) . PHP_EOL;
