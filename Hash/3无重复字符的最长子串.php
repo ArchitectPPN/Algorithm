31575b00835d2bd2ddb9e346113f474e2db1b5af
architectppn <ng_sou@163.com>
2024-06-11 21:57:36 +0800

    重复的字符的最长子串

---
+++ Hash/3无重复字符的最长子串.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HashLengthOfLongestSubstring;
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return int
+     */
+    function lengthOfLongestSubstring($s): int
+    {
+        if (empty($s)) return 0;
+
+        $ans = [];
+        $map = [];
+        $len = strlen($s) - 1;
+        for ($i = 0; $i <= $len; $i++) {
+            if (isset($map[$i])) {
+                $ans = count($ans) >= count($map) ? $ans : $map;
+                $map = [];
+                continue;
+            } else {
+                $map[$s[$i]] = 1;
+                $ans[] = $s[$i];
+            }
+        }
+
+        return count($ans);
+    }
+}
+
+$str = "abcabcbb";
+
+$solution = new Solution();
+echo $solution->lengthOfLongestSubstring($str);
