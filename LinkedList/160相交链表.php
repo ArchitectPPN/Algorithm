e50e97e8b1f10b77a7786cd7e04806a4aaf557d5
architectppn <ng_sou@163.com>
2024-06-17 22:56:40 +0800

    160 876

--- LinkedList/160相交链表.php
+++ LinkedList/160相交链表.php
@@ -2,3 +2,48 @@
 
 # 走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路。
 # 对的人错过了还是会相遇， 错的人相遇了也是NULL。
+
+namespace LinkedListGetIntersectionNode;
+
+class ListNode {
+    /**
+     * @var int
+     */
+    public int $val = 0;
+
+    /**
+     * @var ListNode|null
+     */
+    public ?ListNode $next = null;
+
+    /**
+     * @param int $val
+     */
+    function __construct(int $val) {
+        $this->val = $val;
+    }
+}
+
+class Solution {
+    /**
+     * @param ListNode|null $headA
+     * @param ListNode|null $headB
+     * @return ListNode|null
+     */
+    function getIntersectionNode(?ListNode $headA, ?ListNode $headB): ?ListNode
+    {
+        // 初始化头
+        $pA = $headA;
+        $pB = $headB;
+
+        // 两个节点都走到结尾是null
+        while ($pA !== $pB) {
+            // 如果pA走到结尾后，然后开始接着$headB走
+            // 如果不是null，则继续走pA的节点
+            $pA = $pA == null ? $headB : $pA->next;
+            $pB = $pB == null ? $headA : $pB->next;
+        }
+
+        return $pA;
+    }
+}
--- LinkedList/876链表的中间结点.php
+++ LinkedList/876链表的中间结点.php
@@ -5,3 +5,43 @@
  * 2. 单指针法，进行两次循环，第一次统计链表的长度N，那么第二次走到N/2时，直接返回对应的元素即可
  * 3. 快慢指针法，慢指针一次走一步，快指针一次走两步，那么等快指针走到末尾时，慢指针一定才走到中间的位置，直接返回慢指针指向的元素即可
  */
+
+class ListNode {
+    /**
+     * @var int
+     */
+    public int $val = 0;
+
+    /**
+     * @var ListNode|null
+     */
+    public ?ListNode $next = null;
+
+    function __construct(int $val = 0, ?ListNode $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution {
+    /**
+     * @param ListNode $head
+     * @return ListNode|null
+     */
+    function middleNode(ListNode $head): ?ListNode
+    {
+        // 使用两个快慢指针， 都从头开始
+        $slow = $head;
+        $quick = $head;
+
+        // 快指针一次走两步， 慢指针一次走一步， 等快指针走到尾时， 慢指针刚好走到中间
+        // 此时返回慢指针就可以了
+        // 快指针当前节点和下一个节点不为null时
+        while ($quick != null && $quick->next != null) {
+            $slow = $head->next;
+            $quick = $quick->next->next;
+        }
+
+        return $slow;
+    }
+}
