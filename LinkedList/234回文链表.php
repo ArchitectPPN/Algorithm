46da2c53e378dd7b906bc5ba511ea1bc0a5411a4
architectppn <ng_sou@163.com>
2024-07-24 23:03:54 +0800

    链表转为数组，然后判断是否是回文链表

--- LinkedList/234回文链表.php
+++ LinkedList/234回文链表.php
@@ -1,1 +1,44 @@
 <?php
+
+namespace IsPalindrome;
+
+class ListNode
+{
+    public int $val = 0;
+    public ?ListNode $next = null;
+
+    function __construct(int $val = 0, ?ListNode $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class TranslateArraySolution
+{
+    /**
+     * @param ListNode|null $head
+     * @return Boolean
+     */
+    function isPalindrome(?ListNode $head): bool
+    {
+        if (is_null($head)) {
+            return false;
+        }
+
+        $linkedArr = [];
+        while ($head) {
+            $linkedArr[] = $head;
+            $head = $head->next;
+        }
+
+        $length = count($linkedArr);
+        for ($i = 0; $i < $length / 2; $i++) {
+            if($linkedArr[$i]->val !== $linkedArr[$length - $i - 1]->val) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
