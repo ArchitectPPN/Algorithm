084e2da5a8e48dac865229e59db57bb6052aee68
niujunqing <niujunqing@angelalign.com>
2024-08-25 15:13:10 +0800

    56合并区间

--- Sort/56合并区间.php
+++ Sort/56合并区间.php
@@ -14,12 +14,29 @@ class Solution
      */
     function merge(array $intervals): array
     {
-        return [];
-    }
+        $ans = [];
+        if (empty($intervals)) {
+            return $ans;
+        }
 
-    private function sort(array $intervals)
-    {
+        // 对二维数组进行排序, 升序排序
+        usort($intervals, function ($a, $b) {
+            return $a[0] <=> $b[0];
+        });
 
+        foreach ($intervals as $interval) {
+            $last = end($ans);
+            // 如果最后的结果集为空, 或者当前元素的结束值大于结果集最后一个元素结束值, 说明两个数组不重叠
+            if (empty($last) || $last[1] < $interval[0]) {
+                $ans[] = $interval;
+            } else {
+                // 如果当前值的第一个元素小于等于最后一个元素的结束值，
+                // 则将当前值合并到结果集中, 更新结果集的最后一个元素的结束值
+                $ans[count($ans) - 1][1] = max($last[1], $interval[1]);
+            }
+        }
+
+        return $ans;
     }
 }
 
@@ -45,4 +62,4 @@ $merge = [
 $solution = new Solution();
 $merge = $solution->merge($merge);
 
-var_dump($merge);
+echo json_encode($merge, JSON_UNESCAPED_UNICODE);
