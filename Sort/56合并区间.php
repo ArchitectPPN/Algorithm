e0f3fc8374793fbdaa57caef3b2987a931706f91
niujunqing <niujunqing@angelalign.com>
2024-08-25 14:37:05 +0800

    二维数组排序

--- Sort/56合并区间.php
+++ Sort/56合并区间.php
@@ -2,13 +2,13 @@
 
 namespace ArrayMergeSolution;
 
+# https://leetcode.cn/problems/merge-intervals/description/
 class Solution
 {
     /**
      * 解题思路：
      *  1. 先将数组按照第一个元素进行排序，相同情况下，按照第二个元素进行排序
      *  2. 使用ans数组进行存储结果集，结果集中已经有元素了，检查当前数组的第一个元素是否小于ans最后一个元素结束值
-     *
      * @param Integer[][] $intervals
      * @return Integer[][]
      */
@@ -23,7 +23,24 @@ class Solution
     }
 }
 
-$merge = [[1, 3], [2, 6], [8, 10], [15, 18]];
+$merge = [
+    [
+        1,
+        3,
+    ],
+    [
+        2,
+        6,
+    ],
+    [
+        8,
+        10,
+    ],
+    [
+        15,
+        18,
+    ],
+];
 
 $solution = new Solution();
 $merge = $solution->merge($merge);
--- Sort/二维数组排序.php
+++ Sort/二维数组排序.php
@@ -3,25 +3,74 @@
  * 关联数组排序
  */
 $people = [
-    ['name' => 'John', 'age' => 28],
-    ['name' => 'Jane', 'age' => 22],
-    ['name' => 'Doe', 'age' => 32],
+    [
+        'name' => 'John',
+        'age' => 28,
+    ],
+    [
+        'name' => 'Jane',
+        'age' => 22,
+    ],
+    [
+        'name' => 'Doe',
+        'age' => 32,
+    ],
 ];
+
+var_dump("before sort:", $people);
+
 // 在这个例子中，<=> 运算符（太空船运算符）用于比较两个值并返回一个整数：如果 $a['age'] 小于 $b['age']，则返回 -1；如果相等，则返回 0；如果大于，则返回 1。
 // 经过排序后的 $people 数组将变为：
-/**
- * [
- *   ['name' => 'Jane', 'age' => 22],
- *   ['name' => 'John', 'age' => 28],
- *   ['name' => 'Doe', 'age' => 32],
- * ]
- */
 
 usort($people, function ($a, $b) {
     return $a['age'] <=> $b['age']; // 按年龄升序排序
 });
-
+var_dump("after asc sort:", $people);
 
 /**
- *
+ * 在这个例子中，<=> 运算符（太空船运算符）用于比较两个值，并根据它们的大小关系返回 -1、0 或 1：
+ * 1. 如果 $a['age'] 小于 $b['age']，则返回 -1。
+ * 2. 如果 $a['age'] 等于 $b['age']，则返回 0。
+ * 3. 如果 $a['age'] 大于 $b['age']，则返回 1。
+ * 由于 usort() 函数根据这个返回值来决定数组的排序顺序：
+ * 1. 当返回 -1 时，$a 会排在 $b 之前（即升序）。
+ * 2. 当返回 1 时，$b 会排在 $a 之前（即升序）。
+ * 3. 当返回 0 时，$a 和 $b 的顺序保持不变。
+ * 因此，$a['age'] <=> $b['age'] 这段代码会导致按 年龄升序 进行排序，也就是说，年龄较小的元素会排在前面，年龄较大的元素会排在后面。
  */
+// 如果你想按降序排序，只需要调换 $a 和 $b 的顺序即可：
+usort($people, function ($a, $b) {
+    return $b['age'] <=> $a['age']; // 按年龄降序排序
+});
+var_dump("after desc sort:", $people);
+
+/**
+ * 非关联数组排序
+ */
+$indexArray = [
+    [
+        1,
+        3,
+    ],
+    [
+        8,
+        10,
+    ],
+    [
+        2,
+        6,
+    ],
+    [
+        15,
+        18,
+    ],
+];
+var_dump("before asc sort:", $indexArray);
+usort($indexArray, function ($a, $b) {
+    return $a[0] <=> $b[0]; // 按年龄降序排序
+});
+var_dump("after asc sort:", $indexArray);
+usort($indexArray, function ($a, $b) {
+    return $b[0] <=> $a[0]; // 按年龄降序排序
+});
+var_dump("after desc sort:", $indexArray);
