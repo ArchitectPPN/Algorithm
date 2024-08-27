9bef390759fda7f0d056c15c8edd7c27bcbd8ed7
niujunqing <niujunqing@angelalign.com>
2024-08-27 22:01:59 +0800

    401二进制手表, 枚举解法

--- Enum/401二进制手表.php
+++ Enum/401二进制手表.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace ReadBinaryWatch;
+namespace EnumReadBinaryWatch;
 
+# https://leetcode.cn/problems/binary-watch/description/
+# 解法1: 枚举
+# 解法2: 回溯, 请在回溯目录中查看
 class Solution
 {
     /**
+     * 思路:
+     * 这种思路, 就是枚举每一个小时的每一分钟是否符合条件;
+     * 对于符合条件的组合, 添加到结果数组中.
+     *
+     * 改题目循环的次数和输入的大小是没有关系的, 循环次数是固定的,
+     *
+     *
      * @param Integer $turnedOn
      * @return String[]
      */
@@ -15,7 +25,7 @@ class Solution
         // 遍历所有小时（0-11）和分钟（0-59）的组合
         for ($hour = 0; $hour < 12; $hour++) {
             for ($minute = 0; $minute < 60; $minute++) {
-                // 使用内置的 bit_count 函数来统计 1 的数量
+                // 使用内置的 bitCount 函数来统计 1 的数量
                 if ($this->bitCount($hour) + $this->bitCount($minute) === $turnedOn) {
                     // 格式化时间，确保分钟总是两位数
                     $result[] = sprintf("%d:%02d", $hour, $minute);
