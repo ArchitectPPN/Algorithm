b88fcaceb3e6a8b3807ccf3a09dae2624488c95c
architect <ng_sou@163.com>
2025-03-03 22:39:36 +0800

    Review FindArrMaxSubSequence

--- AlgorithmCategory/Stack/FindArrMaxSubSequence.php
+++ AlgorithmCategory/Stack/FindArrMaxSubSequence.php
@@ -45,7 +45,7 @@ class FindArrMaxSubSequence
                 $top += 1;
                 $stack[$top] = $nowNum;
             } else {
-                // 栈已经满了，丢掉元素
+                // 不需要该元素，意味着要舍弃，所以减1
                 $remain -= 1;
             }
         }
@@ -70,3 +70,63 @@ $arr = [4, 3, 2, 5, 6];
 $k = 3;
 $ans = $svc->maxSubsequence($arr, $k);
 var_dump($ans);
+
+# 2025年3月3日
+
+/**
+ * Thinking
+ * 题目要求从arr中找到k个数字来组成最大的子序列
+ * 因为只要k个数字，所以arrLength - k个数字都可以被移除
+ * 什么样的数字可以被我们移除掉呢？
+ * 首先我们要保证我们的数组是最大的，所以栈顶元素小于当前元素且还可以删除元素时，就应该将栈顶元素丢掉
+ */
+class FindArrMaxSubSequenceReviewOne
+{
+    /**
+     * @param array $arr
+     * @param int $k
+     * @return array
+     */
+    public function maxSubsequence(array $arr, int $k): array
+    {
+        $arrLength = count($arr);
+        // 特殊情况过滤
+        // 给定的数组为空
+        // 给定的数组长度小于k
+        // k为0
+        if (empty($arr) || $k <= 0 || $k > $arrLength) {
+            return [];
+        }
+        $answerStack = array_fill(0, $k, 0);
+        // 可以丢掉的元素个数
+        $remain = $arrLength - $k;
+        // 栈顶元素下标
+        $top = -1;
+
+        for ($i = 0; $i < $arrLength; $i++) {
+            // 获取当前元素
+            $nowNum = $arr[$i];
+            // 栈不为空
+            // 栈顶元素小于当前元素
+            // 还可以删除元素
+            while ($top >= 0 && $answerStack[$top] < $nowNum && $remain > 0) {
+                // 出栈
+                $top -= 1;
+                // 可删除元素-1
+                $remain -= 1;
+            }
+
+            // 栈内元素个数小于k
+            if ($top < $k - 1) {
+                // 入栈
+                $top += 1;
+                $answerStack[$top] = $nowNum;
+            } else {
+                // 不入栈，就意味着要丢掉该元素，所以可删除元素要减1
+                $remain -= 1;
+            }
+        }
+
+        return $answerStack;
+    }
+}
