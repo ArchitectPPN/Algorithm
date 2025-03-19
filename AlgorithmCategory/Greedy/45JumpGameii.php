e3965f2d10f844ed9e4eb6eb4c7040b85dc21cf9
niujunqing <ng_sou@163.com>
2025-03-19 14:54:45 +0800

    45JumpGameII

--- AlgorithmCategory/Greedy/45JumpGameii.php
+++ AlgorithmCategory/Greedy/45JumpGameii.php
@@ -1,3 +1,109 @@
 <?php
 
 # 45. 跳跃游戏 II https://leetcode.cn/problems/jump-game-ii/
+
+class JumpGameIISolution
+{
+    /**
+     * 永远找最远的位置
+     * @param array $ans
+     * @return int
+     */
+    public function jump(array $ans): int
+    {
+        // 数组长度
+        $ansLength = count($ans);
+        // 如果数组长度小于等于 1，无需跳跃，直接返回 0
+        if ($ansLength <= 1) {
+            return 0;
+        }
+        // 步数
+        $step = 0;
+        // 当前跳跃能到达的最远位置
+        $maxPosition = 0;
+        // 当前跳跃覆盖的边界
+        $end = 0;
+
+        for ($i = 0; $i < $ansLength - 1; $i++) {
+            // 更新当前能到达的最远位置
+            $maxPosition = max($maxPosition, $i + $ans[$i]);
+            // 到达当前跳跃覆盖的边界
+            if ($i == $end) {
+                // 增加步数
+                $step++;
+                // 更新下一次跳跃覆盖的边界
+                $end = $maxPosition;
+            }
+        }
+
+        return $step;
+    }
+}
+
+$svc = new JumpGameIISolution();
+echo $svc->jump(
+        [
+            2,
+            3,
+            1,
+            1,
+            4,
+        ]
+    ) . PHP_EOL;
+
+/**
+ * THINKING
+ * 永远找最远的位置, 循环到当前可以达到的最远位置时, 更新跳跃的步数, 然后更新跳跃的边界位置
+ * @author niujunqing
+ */
+class JumpGameIISolutionReviewOne
+{
+    /**
+     * 永远找最远的位置
+     * @param array $ans
+     * @return int
+     */
+    public function jump(array $ans): int
+    {
+        // 数组长度
+        $ansLength = count($ans);
+        // 长度小于等于 1，无需跳跃，直接返回 0
+        if ($ansLength <= 1) {
+            return 0;
+        }
+        // 当前跳跃能到达的最远位置
+        $nowSkipPosition = 0;
+        // 本次跳跃的边界位置
+        $nowSkipEnd = 0;
+        // 步数
+        $step = 0;
+        $skipIndex = [];
+
+        // 因为最后一步不需要跳跃, 所以循环到倒数第二步
+        for ($i = 0; $i < $ansLength - 1; $i++) {
+            // 更新能跳跃到的最远位置
+            $nowSkipPosition = max($nowSkipPosition, $i + $ans[$i]);
+
+            // 循环位置到达跳跃边界时, 更新下一次跳跃边界位置
+            if ($i == $nowSkipEnd) {
+                $skipIndex[] = $i;
+                $nowSkipEnd = $nowSkipPosition;
+                $step++;
+            }
+        }
+
+        var_dump($skipIndex);
+        return $step;
+    }
+}
+
+$svc = new JumpGameIISolutionReviewOne();
+echo $svc->jump(
+        [
+            2,
+            3,
+            1,
+            1,
+            4,
+        ]
+    ) . PHP_EOL;
