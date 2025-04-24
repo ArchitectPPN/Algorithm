e47b01d0f72e2a399b366e33b266435060661e8b
niujunqing <ng_sou@163.com>
2025-04-24 09:45:01 +0800

    200 NumbersOfIslands

--- AlgorithmCategory/Graph/200NumberOfIslands.php
+++ AlgorithmCategory/Graph/200NumberOfIslands.php
@@ -1,3 +1,116 @@
 <?php
 
 # 200. 岛屿数量 https://leetcode.cn/problems/number-of-islands/description/
+
+class NumIslandsSolution
+{
+    /** @var array Islands */
+    private array $grid = [];
+
+    /** @var int rows numbers */
+    private int $nr = 0;
+
+    /** @var int col numbers */
+    private int $nc = 0;
+
+    /**
+     * @param int $row
+     * @param int $col
+     * @return void
+     */
+    private function dfs(int $row, int $col): void
+    {
+        // set visited
+        $this->grid[$row][$col] = '0';
+        // up
+        if ($row - 1 >= 0 && $this->grid[$row - 1][$col] == '1') {
+            $this->dfs($row - 1, $col);
+        }
+        // down
+        if ($row + 1 < $this->nr && $this->grid[$row + 1][$col] == '1') {
+            $this->dfs($row + 1, $col);
+        }
+        // left
+        if ($col - 1 >= 0 && $this->grid[$row][$col - 1] == '1') {
+            $this->dfs($row, $col - 1);
+        }
+        // right
+        if ($col + 1 < $this->nc && $this->grid[$row][$col + 1] == '1') {
+            $this->dfs($row, $col + 1);
+        }
+    }
+
+    /**
+     * @param array $grid
+     * @return int
+     */
+    public function numIslands(array $grid): int
+    {
+        $this->initData($grid);
+        // empty grid
+        if ($this->nr == 0) {
+            return 0;
+        }
+
+        // 岛屿的数量
+        $numberOfIslands = 0;
+
+        for ($row = 0; $row < $this->nr; $row++) {
+            for ($col = 0; $col < $this->nc; $col++) {
+                // island
+                if ($this->grid[$row][$col] == '1') {
+                    $numberOfIslands++;
+                    $this->dfs($row, $col);
+                }
+            }
+        }
+
+        return $numberOfIslands;
+    }
+
+    /**
+     * 初始化数据
+     * @param array $grid
+     * @return void
+     */
+    private function initData(array $grid): void
+    {
+        $this->grid = $grid;
+        $this->nr = count($grid);
+        $this->nc = count($grid[0]);
+    }
+}
+
+$grid = [
+    [
+        "1",
+        "1",
+        "0",
+        "0",
+        "0",
+    ],
+    [
+        "1",
+        "1",
+        "0",
+        "0",
+        "0",
+    ],
+    [
+        "0",
+        "0",
+        "1",
+        "0",
+        "0",
+    ],
+    [
+        "0",
+        "0",
+        "0",
+        "1",
+        "1",
+    ],
+];
+
+$svc = new NumIslandsSolution();
+echo $svc->numIslands($grid);
