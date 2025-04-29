37938cb3ad0ec5aa8ee96b01954580761c2e62b0
niujunqing <ng_sou@163.com>
2025-04-29 17:09:04 +0800

    207 深度优先遍历

--- AlgorithmCategory/Graph/207CourseSchedule.php
+++ AlgorithmCategory/Graph/207CourseSchedule.php
@@ -2,7 +2,7 @@
 
 # 207. 课程表 https://leetcode.cn/problems/course-schedule/
 
-class CanFinishSolution
+class BFSCanFinishSolution
 {
     // 存储图的边信息, $edges[i] 表示从节点 i 出发的边的目标节点列表
     private array $edges = [];
@@ -71,8 +71,97 @@ $questions = [
     [2, [[1, 0]]],
 ];
 
-$svc = new CanFinishSolution();
+$svc = new BFSCanFinishSolution();
 foreach ($questions as $question) {
+    $res = $svc->canFinish($question[0], $question[1]);
+    var_dump($res);
+}
+
+
+$questions = [
+    [2, [[1, 0], [0, 1]]],
+    [2, [[1, 0]]],
+];
+class DFSCanFinishSolution
+{
+    /** @var array 存储图的边信息，$edges[$i] 表示从节点 i 出发的边的目标节点列表 */
+    private array $edges = [];
+    /** @var array 存储节点的访问状态，0 表示未访问，1 表示正在访问（在当前递归路径中），2 表示已访问过且不在当前递归路径中 */
+    private array $visited = [];
+    /** @var bool 用于标记是否存在环，初始为 true，若发现环则设为 false */
+    private bool $valid = true;
+
+    /**
+     * 深度优先搜索方法
+     * @param int $u 节点编号
+     * @return void
+     */
+    private function dfs(int $u): void
+    {
+        // 将节点 u 标记为正在访问
+        $this->visited[$u] = 1;
+        // 遍历从节点 u 出发的所有边的目标节点 v
+        foreach ($this->edges[$u] as $v) {
+            // 如果节点 v 未被访问
+            if ($this->visited[$v] === 0) {
+                // 对节点 v 进行深度优先搜索
+                $this->dfs($v);
+                // 如果已经发现存在环，直接返回
+                if (!$this->valid) {
+                    return;
+                }
+            } elseif ($this->visited[$v] === 1) {
+                // 如果节点 v 正在被访问（在当前递归路径中），说明存在环
+                $this->valid = false;
+                return;
+            }
+        }
+        // 将节点 u 标记为已访问过且不在当前递归路径中
+        $this->visited[$u] = 2;
+    }
+
+    /**
+     * 判断是否可以完成所有课程的方法
+     * @param int $numCourses 课程数量
+     * @param array $prerequisites 课程先决条件列表
+     * @return bool 如果可以完成所有课程返回 true，否则返回 false
+     */
+    public function canFinish(int $numCourses, array $prerequisites): bool
+    {
+        // 输入验证：检查先决条件列表中的元素是否为长度为 2 的数组
+        foreach ($prerequisites as $info) {
+            if (!is_array($info) || count($info) !== 2) {
+                throw new InvalidArgumentException("每个先决条件必须是长度为 2 的数组");
+            }
+        }
+
+        // 调整 $edges 数组的大小以适应课程数量
+        for ($i = 0; $i < $numCourses; $i++) {
+            $this->edges[$i] = [];
+        }
+        // 初始化每个节点的访问状态为未访问（0）
+        $this->visited = array_fill(0, $numCourses, 0);
+
+        // 遍历先决条件列表，构建图的边信息
+        // 对于每个先决条件 [a, b]，表示学习课程 a 前必须先学习课程 b
+        // 所以从节点 b 出发有一条指向节点 a 的边
+        foreach ($prerequisites as $info) {
+            $this->edges[$info[1]][] = $info[0];
+        }
+
+        // 对每个未访问的节点进行深度优先搜索
+        for ($i = 0; $i < $numCourses && $this->valid; $i++) {
+            if ($this->visited[$i] === 0) {
+                $this->dfs($i);
+            }
+        }
+
+        // 如果不存在环，返回 true；否则返回 false
+        return $this->valid;
+    }
+}
+foreach ($questions as $question) {
+    $svc = new DFSCanFinishSolution();
     $res = $svc->canFinish($question[0], $question[1]);
     var_dump($res);
 }
