766fde2e1e0578c96927ada6abae3c5fe149330f
architect <ng_sou@163.com>
2025-06-08 18:25:44 +0800

    1 TwoSum

--- AlgorithmCategory/Hash/1TwoSum.php
+++ AlgorithmCategory/Hash/1TwoSum.php
@@ -2,3 +2,34 @@
 
 # 1. 两数之和 https://leetcode.cn/problems/two-sum/description/
 
+class SolutionWithHash
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum(array $nums, int $target): array
+    {
+        $hashTable = [];
+        foreach ($nums as $index => $value) {
+            if (isset($hashTable[$target - $value])) {
+                return [$hashTable[$target - $value], $index];
+            }
+            $hashTable[$value] = $index;
+        }
+        return [];
+    }
+}
+
+$questions = [
+    [[2, 7, 11, 15], 9],
+    [[3, 2, 4], 6],
+    [[3, 3], 6],
+];
+
+$svc = new SolutionWithHash();
+foreach ($questions as $question) {
+    $ans = $svc->twoSum($question[0], $question[1]);
+    var_dump($ans);
+}
