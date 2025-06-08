c82db249c87d6e00a9c3dd3cecd9fb18fd4c5db2
architect <ng_sou@163.com>
2025-06-08 20:54:55 +0800

    49 GroupAnagramsSolution

---
+++ AlgorithmCategory/Hash/49GroupAnagrams.php
@@ -0,0 +1,15 @@
+<?php
+
+# 49. 字母异位词分组 https://leetcode.cn/problems/group-anagrams/description/
+
+class GroupAnagramsSolution
+{
+    /**
+     * @param String[] $strs
+     * @return String[][]
+     */
+    function groupAnagrams(array $strs): array
+    {
+        return [];
+    }
+}
