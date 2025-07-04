8de44c097e326dad95dd87fe2b25eadf06282015
niujunqing <ng_sou@163.com>
2025-07-04 13:52:01 +0800

    19 RemoveNthNodeFromEndOfListWithStack

---
+++ AlgorithmCategory/LinkedList/19RemoveNthNodeFromEndOfList/19RemoveNthNodeFromEndOfListWithStack.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RemoveNthNodeFromEndOfList;
+
+class RemoveNthNodeFromEndOfListWithStack
+{
+    /*
+     * 用栈删除
+     */
+    function removeNthFromEnd(?ListNode $head, int $n): ?ListNode
+    {
+        if (is_null($head)) {
+            return null;
+        }
+
+        $dummy = new ListNode(0);
+        $dummy->next = $head;
+
+        // 入栈
+        $stack = [];
+        $current = $dummy; // 从dummy开始入栈
+        while ($current) {
+            $stack[] = $current;
+            $current = $current->next;
+        }
+
+        // n大于链表长度, 说明要删除的节点不在数组中, 直接返回头节点
+        if ($n > count($stack)) {
+            return $dummy->next;
+        }
+
+        $preNode = new ListNode();
+        // 出栈找到要删除节点的前一个节点
+        for ($i = 0; $i <= $n; $i++) {
+            $preNode = array_pop($stack);
+        }
+
+        $preNode->next = $preNode->next->next;
+
+        return $dummy->next;
+    }
+}
